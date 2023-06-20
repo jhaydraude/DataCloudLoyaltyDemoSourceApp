166692b92a915a83be81efb371eb95a9077476b6
jhaydraude <jhaydraude@salesforce.com>
2023-06-20 10:58:18 -0400

    FixSampleData

---
+++ SampleData/LoadData.sh
@@ -0,0 +1,2 @@
+sf data upsert bulk --sobject Contact --file ./SampleData/DataCloudSampleContacts.csv --external-id id
+sf data upsert bulk --sobject Loyalty_Profile__c --file ./SampleData/DataCloudSampleLoyalty.csv --external-id id
