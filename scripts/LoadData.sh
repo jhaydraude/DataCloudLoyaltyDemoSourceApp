47f52ff4eea5b99aaabca7de2804baf08cbed1dd
jhaydraude <jhaydraude@salesforce.com>
2023-06-23 17:01:09 -0400

    Update Scripts

--- scripts/LoadData.sh
+++ scripts/LoadData.sh
@@ -1,6 +1,9 @@
-sfdx auth:web:login -a dcwksorg
-sfdx force:mdapi:deploy  -d metadataPackage -u dcwksorg -w 10
-sfdx force:user:permset:assign -n Loyalty -u dcwksorg
-sfdx force:user:permset:assign -n CDPAdmin -u dcwksorg
-sf data upsert bulk --sobject Contact --file ./SampleData/DataCloudSampleContacts.csv --external-id id -u dcwksorg
-sf data upsert bulk --sobject Loyalty_Profile__c --file ./SampleData/DataCloudSampleLoyalty.csv --external-id id -u dcwksorg
+export usernum=01
+sfdx auth:web:login -a dcwksorg$USERNUM
+sfdx force:mdapi:deploy  -d metadataPackage -u dcwksorg$USERNUM -w 10
+sfdx force:user:permset:assign -n Loyalty -u dcwksorg$USERNUM
+sfdx force:user:permset:assign -n CDPAdmin -u dcwksorg$USERNUM
+sf data upsert bulk --sobject Contact --file ./SampleData/DataCloudSampleContacts.csv --external-id id -u dcwksorg$USERNUM
+sf data upsert bulk --sobject Loyalty_Profile__c --file ./SampleData/DataCloudSampleLoyalty.csv --external-id id -u dcwksorg$USERNUM
+
+
